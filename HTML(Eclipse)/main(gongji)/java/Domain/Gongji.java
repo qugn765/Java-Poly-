af156f44103edd9beed492d51244b764fc36c9a9
qugn765 <qugn765@naver.com>
2023-06-13 17:25:55 +0900

    2023.06.13

---
+++ HTML(Eclipse)/main(gongji)/java/Domain/Gongji.java
@@ -0,0 +1,33 @@
+package Domain;
+
+public class Gongji {
+	int id;
+	String title;
+	String date;
+	String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
