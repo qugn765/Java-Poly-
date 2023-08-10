1142b8e9c761e46affa97b91be039e418f31d5de
qugn765 <qugn765@naver.com>
2023-08-11 08:56:47 +0900

    add

---
+++ HTML(Eclipse)/main(market)/java/Domain/Item.java
@@ -0,0 +1,61 @@
+package Domain;
+
+public class Item {
+	int id;
+	String name;
+	int inventory;
+	String goodsdate;
+	String inventorydate;
+	String explanation;
+	String img1;
+	String img2;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getInventory() {
+		return inventory;
+	}
+	public void setInventory(int inventory) {
+		this.inventory = inventory;
+	}
+	public String getGoodsdate() {
+		return goodsdate;
+	}
+	public void setGoodsdate(String goodsdate) {
+		this.goodsdate = goodsdate;
+	}
+	public String getInventorydate() {
+		return inventorydate;
+	}
+	public void setInventorydate(String inventorydate) {
+		this.inventorydate = inventorydate;
+	}
+	public String getExplanation() {
+		return explanation;
+	}
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
+	public String getImg1() {
+		return img1;
+	}
+	public void setImg1(String img1) {
+		this.img1 = img1;
+	}
+	public String getImg2() {
+		return img2;
+	}
+	public void setImg2(String img2) {
+		this.img2 = img2;
+	}
+
+}
---
+++ HTML(Eclipse)/main(market)/java/Service/Service.java
@@ -0,0 +1,14 @@
+package Service;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import Dto.Pagination;
+
+public interface Service {
+
+
+	public Pagination getPagination(int page, int countPerPage) throws ClassNotFoundException, SQLException;
+	
+	LocalDate nowTime();
+}
